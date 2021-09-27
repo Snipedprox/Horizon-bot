f6b6a4274658d958850f14a294b1fa5cc23fd039
Snipedprox <85262576+Snipedprox@users.noreply.github.com>
2021-09-27 15:32:03 -0300

    Update eval.js

--- Commands/dev/eval.js
+++ Commands/dev/eval.js
@@ -1,1 +1,8 @@
+module.exports = {
+name: "eval",
+aliases: ['aoi' , 'ev'],
+code:`
+$onlyForids[$botownerid;870441674525012018;only my  owners may use this command]
+$eval[$message]
+$deletecommand`}
 
