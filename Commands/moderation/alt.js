7996b646641f40e1f64d9cec99627f8d5e733dc9
Snipedprox <85262576+Snipedprox@users.noreply.github.com>
2021-09-27 15:30:51 -0300

    Update alt.js

--- Commands/moderation/alt.js
+++ Commands/moderation/alt.js
@@ -1,1 +1,14 @@
-
+module.exports = {
+name: "banalt",
+aliases: ['balt' , 'bana'],
+code:`
+$if[$creationdate[$mentioned[1];time]>30]
+$ban[$mentioned[1]]
+$username[$mentioned[1]]#$discriminator[$mentioned[1]] has been banned from $servername because there account was younger than 30d
+$deletecommand
+their account is $creationdate[$mentioned[1];time] old
+$else
+sorry $username that user is older than 30d old
+$deletecommand
+$endif`
+}
